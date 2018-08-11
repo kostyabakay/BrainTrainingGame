bc269d9ecd72b0e66f6d10bffe1662bb4f92c584
Kostya Bakay <kostya.bakay@gmail.com>
2018-08-12 02:33:56 +0300

    Code refactoring for SettingsFragment

--- app/src/main/java/com/kostyabakay/braintraininggame/activity/SettingsActivity.java
+++ app/src/main/java/com/kostyabakay/braintraininggame/activity/SettingsActivity.java
@@ -1,7 +1,6 @@
 package com.kostyabakay.braintraininggame.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import com.kostyabakay.braintraininggame.R;
 import com.kostyabakay.braintraininggame.fragment.SettingsFragment;
@@ -10,13 +9,14 @@ import com.kostyabakay.braintraininggame.fragment.SettingsFragment;
  * Created by Kostya on 06.03.2016.
  * This class represents Activity for settings of the game.
  */
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends BaseActivity {
 
+    //region BaseActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_settings);
-        getFragmentManager().beginTransaction()
-                .replace(R.id.settings_content_frame, new SettingsFragment()).commit();
+        replaceFragment(R.id.settings_content_frame, SettingsFragment.newInstance());
     }
+    //endregion
 }
