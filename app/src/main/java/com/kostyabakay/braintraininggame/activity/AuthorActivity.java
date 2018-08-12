6a8cedb0f73243429f3ae918883c19a5fd48d7bc
Kostya Bakay <kostya.bakay@gmail.com>
2018-08-12 03:26:52 +0300

    Code refactoring for AuthorActivity

--- app/src/main/java/com/kostyabakay/braintraininggame/activity/AuthorActivity.java
+++ app/src/main/java/com/kostyabakay/braintraininggame/activity/AuthorActivity.java
@@ -5,21 +5,14 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.ImageView;
 
 import com.kostyabakay.braintraininggame.R;
+import com.kostyabakay.braintraininggame.constant.SocialUrl;
 
-/**
- * Created by Kostya on 08.03.2016.
- * This class represents Activity with information about author of this application.
- */
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class AuthorActivity extends BaseActivity {
-    private static final String TWITTER_URL = "https://twitter.com/Kostya_Bakay";
-    private static final String INSTAGRAM_URL = "https://instagram.com/kostya_bakay";
-    private static final String GITHUB_URL = "https://github.com/kostyabak";
-    private static final String LINKEDIN_URL = "https://ua.linkedin.com/in/kostyabakay";
 
     public static void start(@NonNull Context context) {
         Intent starter = new Intent(context, AuthorActivity.class);
@@ -31,57 +24,29 @@ public class AuthorActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_author);
-        initView();
+        ButterKnife.bind(this);
     }
     //endregion
 
-    /**
-     * Initialization view elements on the screen.
-     */
-    private void initView() {
-        ImageView twitterImageView = (ImageView) findViewById(R.id.twitter_image);
-        ImageView instagramImageView = (ImageView) findViewById(R.id.instagram_image);
-        ImageView githubImageView = (ImageView) findViewById(R.id.github_image);
-        ImageView linkedinImageView = (ImageView) findViewById(R.id.linkedin_image);
-
-        twitterImageView.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent twitterIntent = new Intent();
-                twitterIntent.setAction(Intent.ACTION_VIEW);
-                twitterIntent.addCategory(Intent.CATEGORY_BROWSABLE);
-                twitterIntent.setData(Uri.parse(TWITTER_URL));
-                startActivity(twitterIntent);
-            }
-        });
-
-        instagramImageView.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent instagramIntent = new Intent();
-                instagramIntent.setAction(Intent.ACTION_VIEW);
-                instagramIntent.addCategory(Intent.CATEGORY_BROWSABLE);
-                instagramIntent.setData(Uri.parse(INSTAGRAM_URL));
-                startActivity(instagramIntent);
-            }
-        });
-
-        githubImageView.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent githubIntent = new Intent();
-                githubIntent.setAction(Intent.ACTION_VIEW);
-                githubIntent.addCategory(Intent.CATEGORY_BROWSABLE);
-                githubIntent.setData(Uri.parse(GITHUB_URL));
-                startActivity(githubIntent);
-            }
-        });
-
-        linkedinImageView.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent linkedinIntent = new Intent();
-                linkedinIntent.setAction(Intent.ACTION_VIEW);
-                linkedinIntent.addCategory(Intent.CATEGORY_BROWSABLE);
-                linkedinIntent.setData(Uri.parse(LINKEDIN_URL));
-                startActivity(linkedinIntent);
-            }
-        });
+    //region ButterKnife
+    @OnClick(R.id.twitter_image)
+    void onTwitterClick() {
+        openWebPage(Uri.parse(SocialUrl.TWITTER));
     }
+
+    @OnClick(R.id.instagram_image)
+    void onInstagramClick() {
+        openWebPage(Uri.parse(SocialUrl.INSTAGRAM));
+    }
+
+    @OnClick(R.id.github_image)
+    void onGitHubClick() {
+        openWebPage(Uri.parse(SocialUrl.GITHUB));
+    }
+
+    @OnClick(R.id.linkedin_image)
+    void onLinkedInClick() {
+        openWebPage(Uri.parse(SocialUrl.LINKED_IN));
+    }
+    //endregion
 }
---
+++ app/src/main/java/com/kostyabakay/braintraininggame/constant/SocialUrl.java
@@ -0,0 +1,11 @@
+package com.kostyabakay.braintraininggame.constant;
+
+public final class SocialUrl {
+    private SocialUrl() {
+    }
+
+    public static final String TWITTER = "https://twitter.com/kostya_bakay";
+    public static final String INSTAGRAM = "https://instagram.com/kostya_bakay";
+    public static final String GITHUB = "https://github.com/kostyabakay";
+    public static final String LINKED_IN = "https://ua.linkedin.com/in/kostyabakay";
+}
