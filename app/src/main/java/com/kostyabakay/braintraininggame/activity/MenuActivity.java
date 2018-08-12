194ecb09d4933e645ec5679bdc650b71a0a26b0d
Kostya Bakay <kostya.bakay@gmail.com>
2018-08-12 03:05:26 +0300

    Implemented ButterKnife for MenuActivity

--- app/src/main/java/com/kostyabakay/braintraininggame/activity/MenuActivity.java
+++ app/src/main/java/com/kostyabakay/braintraininggame/activity/MenuActivity.java
@@ -1,57 +1,47 @@
 package com.kostyabakay.braintraininggame.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import com.kostyabakay.braintraininggame.R;
 
-/**
- * Created by Kostya on 04.03.2016.
- * This class represents Activity for game menu. This is the main Activity of application.
- */
-public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
+public class MenuActivity extends BaseActivity {
+
+    //region BaseActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        initView();
+        ButterKnife.bind(this);
+    }
+    //endregion
+
+    //region ButterKnife
+    @OnClick(R.id.menu_play_button)
+    void onPlayButtonClick() {
+        GameActivity.start(this);
     }
 
-    /**
-     * Initialization view elements on the screen.
-     */
-    private void initView() {
-        findViewById(R.id.menu_play_button).setOnClickListener(this);
-        findViewById(R.id.menu_rules_button).setOnClickListener(this);
-        findViewById(R.id.menu_author_button).setOnClickListener(this);
-        findViewById(R.id.menu_settings_button).setOnClickListener(this);
-        findViewById(R.id.menu_exit_button).setOnClickListener(this);
+    @OnClick(R.id.menu_rules_button)
+    void onRulesButtonClick() {
+        RulesActivity.start(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.menu_play_button:
-                GameActivity.start(this);
-                break;
-
-            case R.id.menu_rules_button:
-                RulesActivity.start(this);
-                break;
-
-            case R.id.menu_author_button:
-                AuthorActivity.start(this);
-                break;
-
-            case R.id.menu_settings_button:
-                SettingsActivity.start(this);
-                break;
-
-            case R.id.menu_exit_button:
-                finish(); // Exit from the app
-                break;
-        }
+    @OnClick(R.id.menu_author_button)
+    void onAuthorButtonClick() {
+        AuthorActivity.start(this);
     }
+
+    @OnClick(R.id.menu_settings_button)
+    void onSettingsButtonClick() {
+        SettingsActivity.start(this);
+    }
+
+    @OnClick(R.id.menu_exit_button)
+    void onExitButtonClick() {
+        finish();
+    }
+    //endregion
 }
